8edf73cc6efff11a540a12ec2c0ac07af898858a
JonPulat-099 <rjpm023@gmail.com>
2025-01-29 05:12:22 +0500

    feat: create sheet for organization

--- sheets/admin.py
+++ sheets/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import OrganizationCategory, Organization, Vacancy, Candidate, Config
 
+
 @admin.register(Config)
 class ConfigAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Config._meta.fields]
+
 
 @admin.register(OrganizationCategory)
 class OrganizationCategoryAdmin(admin.ModelAdmin):
@@ -12,7 +14,8 @@ class OrganizationCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'org_code', 'category', 'logo', 'created_at')
+    list_display = ('name', 'org_code', 'category', 'logo', 'created_at', 'vacancy_sheet_url', 'candidate_sheet_url')
+    exclude = ('vacancy_sheet_url', 'candidate_sheet_url')
 
 
 @admin.register(Vacancy)
@@ -21,8 +24,9 @@ class Vacancy(admin.ModelAdmin):
     list_filter = ['created_at', 'organization',
                    'salary']  # Example for adding filters
     search_fields = ['title', 'organization__name']
-    
+
     change_list_template = 'vacancy/changelist.html'
+
 
 @admin.register(Candidate)
 class CandidateAdmin(admin.ModelAdmin):
--- sheets/models.py
+++ sheets/models.py
@@ -2,6 +2,8 @@ import os
 from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import post_delete
+from sheets.services.google.auth import spreadsheet_service, drive_service
+from googleapiclient.errors import HttpError
 
 
 class Config(models.Model):
@@ -11,6 +13,7 @@ class Config(models.Model):
 
     def __str__(self):
         return self.key
+
 
 class OrganizationCategory(models.Model):
     name = models.CharField(max_length=100)
@@ -24,6 +27,8 @@ class OrganizationCategory(models.Model):
 class Organization(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    vacancy_sheet_url = models.URLField(null=True, blank=True)
+    candidate_sheet_url = models.URLField(null=True, blank=True)
     org_code = models.CharField(max_length=100, unique=True, null=False)
     category = models.ForeignKey(
         OrganizationCategory, on_delete=models.CASCADE)
@@ -38,6 +43,33 @@ class Organization(models.Model):
 
     def save(self, *args, **kwargs):
         try:
+            requests = []
+            requests.append({
+                'addSheet': {
+                    'properties': {
+                        'title': f"{self.org_code}_vacancies",
+                    },
+                }
+            })
+            requests.append({
+                'addSheet': {
+                    'properties': {
+                        'title': f"{self.org_code}_candidates"
+                    },
+                }
+            })
+
+            body = {"requests": requests}
+            sheet = spreadsheet_service.spreadsheets().batchUpdate(
+                spreadsheetId=Config.objects.get(key='sheet_id').value, body=body).execute()
+            replies = sheet.get('replies')
+
+            self.vacancy_sheet_url = f"https://docs.google.com/spreadsheets/d/{Config.objects.get(key='sheet_id').value}/edit#gid={replies[0]['addSheet']['properties']['sheetId']}"
+            self.candidate_sheet_url = f"https://docs.google.com/spreadsheets/d/{Config.objects.get(key='sheet_id').value}/edit#gid={replies[1]['addSheet']['properties']['sheetId']}"
+        except HttpError as err:
+            print(err)
+
+        try:
             this = Organization.objects.get(id=self.id)
             if this.logo != self.logo:
                 if this.logo:
@@ -49,7 +81,8 @@ class Organization(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 @receiver(post_delete, sender=Organization)
 def delete_image_file(sender, instance, **kwargs):
     if instance.logo and instance.logo.storage.exists(instance.logo.name):
@@ -66,7 +99,8 @@ class Vacancy(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+
 class Candidate(models.Model):
     email = models.EmailField(max_length=100)
     full_name = models.CharField(max_length=100)
