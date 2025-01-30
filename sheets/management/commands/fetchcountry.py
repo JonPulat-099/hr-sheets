408627a10db9223f54b507e8272f5ba56c259b83
JonPulat-099 <rjpm023@gmail.com>
2025-01-30 11:02:47 +0500

    feat: fetch & save country to db

--- sheets/management/commands/fetchcountry.py
+++ sheets/management/commands/fetchcountry.py
@@ -1,13 +1,28 @@
+import requests
 from django.core.management.base import BaseCommand, CommandError
+from sheets.services.restcountries.save_contry_data import save_data
 
 
 class Command(BaseCommand):
-    help = "Create a Google Sheet"
-
-    # TODO: try|catch block and handle exceptions
+    help = "Fetch & save country data"
 
     def handle(self, *args, **options):
         self.stdout.write("🟢 Command runned successfully")
-        
+        try:
+            url = f"https://restcountries.com/v3.1/all"
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                data = response.json()
+                print(f"Fetched {len(data)} countries")
+                for country in data:
+                    # print(country.get("name").get("common"))
+                    save_data(country)
+
+            else:
+                self.stdout.write(f"❌ Failed to fetch data, status code: {response.status_code}")
+
+        except Exception as e:
+            self.stdout.write(f"❌ Failed to fetch data: {e}")
 
         self.stdout.write("🔵 Command completed")
